f140cbc6f07f439f672cc90a5bf4c4b1b0002e9d
sadumina <it23394124@my.sliit.lk>
2026-01-26 09:49:19 +0530

    add feat:main.py

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from "axios";
+
 import "./styles/theme-haycarb-white.css";
 import App from './App.jsx'
-
 import { ThemeProvider } from "./context/ThemeContext";
+
+// ✅ GLOBAL BACKEND CONFIG (ONE PLACE ONLY)
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider> 
       <App />
     </ThemeProvider>
-   
   </StrictMode>,
 )
